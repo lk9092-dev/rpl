326fe44b11a49cf48b8745b2e29bf151875df08b
lk9092-dev <lk9092@srmist.edu.in>
2025-09-09 15:37:57 +0530

    node creation

--- src/my_robot_controller/my_robot_controller/my_first_node.py
+++ src/my_robot_controller/my_robot_controller/my_first_node.py
@@ -1,1 +1,12 @@
-print("hello")
+import rclpy
+import fromrclpy.node import node
+
+class mynode(node):
+        def _init_(self):
+            super()._init_("first_node")
+def main(args=None):
+    rclpy.init(args=args)
+
+    rclpy.shutdown()
+if_name_=='_main_':
+    main()
